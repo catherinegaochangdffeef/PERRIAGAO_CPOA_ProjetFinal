ff63701bb200fa97ed10015ea3e7323c39626e4a
catherinegaochangdffeef <catherinegaochang@gmail.com>
2020-10-24 18:24:03 +0200

    Changé une petite erreur dans FenetreProduit.fxml (nom de controller)

--- src/Application/MainInterface.java
+++ src/Application/MainInterface.java
@@ -14,7 +14,7 @@ public class MainInterface extends Application {
 	@Override
 	public void start(Stage primaryStage){
 		try {
-		Parent root = FXMLLoader.load(getClass().getResource("Fenetreproduit.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("Fenetreproduit.fxml"));
 			//Parent root = FXMLLoader.load(getClass().getResource("FenetreAccueil.fxml"));
 			Scene scene = new Scene((VBox) root, 600, 400);		
 			primaryStage.setScene(scene);
